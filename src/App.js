ed50fde351ec3911c003907332af77b299892e60
Matías Rojas Rojas <69127196+matias-rojas-dev@users.noreply.github.com>
2021-05-27 00:07:50 -0400

    Implementación de sección para grafos no dirigídos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 import Main from './components/Main';
 import Header from './components/Header';
+import MainNotDirected from './components/MainNotDirected';
+import InitialPage from './components/InitialPage';
 
 const App = () => (
   <BrowserRouter>
     <Header />
-    <Route path='/' component={Main} >
-        <Main />
-    </Route>
+    <Switch>
+    <Route exact path='/' component={InitialPage} />
+    <Route exact path='/dirigido' component={Main} />
+    <Route exact path='/no-dirigido' component={MainNotDirected} />
+    </Switch>
+    
+
   </BrowserRouter>
 
 )
